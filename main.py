1b082f0dcd226947707604a934e295b9b5cd6cee
Dominika Piątek <skala90@gmail.com>
2020-04-05 20:03:28 +0200

    ćwiczenie 3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -21,3 +22,27 @@ def method(request: Request):
 @app.delete("/method/")
 def method(request: Request):
     return {"method": f"{request.method}"}
+
+class PatientInput(BaseModel):
+    name: str
+    surename: str
+
+class PatientOutput(BaseModel):
+    id: int
+    patient: dict
+
+def update_counter():
+    with open("counter.txt",mode="r") as f:
+        counter = f.read()
+
+    counter = int(counter)
+
+    with open("counter.txt",mode="w") as f:
+        f.write(str(counter+1))
+
+    return counter
+
+
+@app.post("/patient/", response_model=PatientOutput)
+def receive_patient(rq: PatientInput):
+    return PatientOutput(patient=rq.dict(), id=update_counter())
