abe4c15667321503be256c40db32ac1489ab57ab
Dominika Piątek <skala90@gmail.com>
2020-04-05 20:43:45 +0200

    ćwiczenie 4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
+import json
 
 app = FastAPI()
 
@@ -42,7 +43,33 @@ def update_counter():
 
     return counter
 
+def load_patient_db():
+    with open('patient_data.jsonl', 'r', encoding='utf') as infile:
+        jsonl_content = infile.readlines()
+    return [json.loads(item) for item in jsonl_content]
+
+
+def save_patient(PatientOutput):
+    with open('patient_data.jsonl', 'a', encoding='utf') as outfile:
+        res_dict = PatientOutput.patient
+        res_dict["id"] = PatientOutput.id
+        json.dump(res_dict, outfile, ensure_ascii=False)
+        outfile.write('\n')
+
 
 @app.post("/patient/", response_model=PatientOutput)
 def receive_patient(rq: PatientInput):
-    return PatientOutput(patient=rq.dict(), id=update_counter())
+    result = PatientOutput(patient=rq.dict(), id=update_counter())
+    save_patient(result)
+    return result
+
+@app.get("/patient/{id}")
+def find_patient(id):
+    list_of_patients = load_patient_db()
+    print(list_of_patients)
+    res = [item for item in list_of_patients if item["id"] == int(id)]
+    print(res)
+    if len(res) == 1:
+        return {"name":res[0]["name"], "surename":res[0]["surename"]}
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
