288782ef454667e768716781e1f1825fde3b0c98
Dominika Piątek <skala90@gmail.com>
2020-04-05 19:32:30 +0200

    ćwiczenie 2

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.get("/method/")
+def method(request: Request):
+    return {"method": f"{request.method}"}
