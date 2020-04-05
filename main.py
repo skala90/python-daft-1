514ecfde57e915a551fd6359679ea61e43299db5
Dominika Piątek <skala90@gmail.com>
2020-04-05 19:37:35 +0200

    ćwiczenie 2 cd

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ def root():
 @app.get("/method/")
 def method(request: Request):
     return {"method": f"{request.method}"}
+
+@app.put("/method/")
+def method(request: Request):
+    return {"method": f"{request.method}"}
+
+@app.post("/method/")
+def method(request: Request):
+    return {"method": f"{request.method}"}
+
+@app.delete("/method/")
+def method(request: Request):
+    return {"method": f"{request.method}"}
