1643b4afad43ba8d4de2380b2dbb195f9a737e35
Dominika Piątek <skala90@gmail.com>
2020-04-26 18:38:24 +0200

    ćwiczenie 3

--- main.py
+++ main.py
@@ -12,57 +12,66 @@ app.tokens = []
 
 security = HTTPBasic()
 
+
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
 
 @app.get("/method/")
 def method(request: Request):
     return {"method": f"{request.method}"}
 
+
 @app.put("/method/")
 def method(request: Request):
     return {"method": f"{request.method}"}
+
 
 @app.post("/method/")
 def method(request: Request):
     return {"method": f"{request.method}"}
 
+
 @app.delete("/method/")
 def method(request: Request):
     return {"method": f"{request.method}"}
+
 
 class PatientInput(BaseModel):
     name: str
     surename: str
 
+
 class PatientOutput(BaseModel):
     id: int
     patient: dict
 
+
 def update_counter():
-    with open("counter.txt",mode="r") as f:
+    with open("counter.txt", mode="r") as f:
         counter = f.read()
 
     counter = int(counter)
 
-    with open("counter.txt",mode="w") as f:
-        f.write(str(counter+1))
+    with open("counter.txt", mode="w") as f:
+        f.write(str(counter + 1))
 
     return counter
 
+
 def load_patient_db():
-    with open('patient_data.jsonl', 'r', encoding='utf') as infile:
+    with open("patient_data.jsonl", "r", encoding="utf") as infile:
         jsonl_content = infile.readlines()
     return [json.loads(item) for item in jsonl_content]
 
 
 def save_patient(PatientOutput):
-    with open('patient_data.jsonl', 'a', encoding='utf') as outfile:
+    with open("patient_data.jsonl", "a", encoding="utf") as outfile:
         res_dict = PatientOutput.patient
         res_dict["id"] = PatientOutput.id
         json.dump(res_dict, outfile, ensure_ascii=False)
-        outfile.write('\n')
+        outfile.write("\n")
 
 
 @app.post("/patient/", response_model=PatientOutput)
@@ -71,6 +80,7 @@ def receive_patient(rq: PatientInput):
     save_patient(result)
     return result
 
+
 @app.get("/patient/{id}")
 def find_patient(id):
     list_of_patients = load_patient_db()
@@ -78,16 +88,20 @@ def find_patient(id):
     res = [item for item in list_of_patients if item["id"] == int(id)]
     print(res)
     if len(res) == 1:
-        return {"name":res[0]["name"], "surename":res[0]["surename"]}
+        return {"name": res[0]["name"], "surename": res[0]["surename"]}
     else:
         raise HTTPException(status_code=204, detail="Item not found")
 
+
 @app.get("/welcome")
 def welcome():
-	return {"message": "Hello World during the coronavirus pandemic!"}
+    return {"message": "Hello World during the coronavirus pandemic!"}
+
 
 @app.post("/login")
-def get_current_username(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
+def get_current_username(
+    response: Response, credentials: HTTPBasicCredentials = Depends(security)
+):
     correct_username = secrets.compare_digest(credentials.username, "trudnY")
     correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
     if not (correct_username and correct_password):
@@ -97,16 +111,19 @@ def get_current_username(response: Response, credentials: HTTPBasicCredentials =
             headers={"WWW-Authenticate": "Basic"},
         )
     else:
-    	s_token = sha256(bytes(f"{correct_username}{correct_password}", encoding='utf8')).hexdigest()
-    	app.tokens += s_token
-    	response.set_cookie(key="session_token",value=s_token)
-    	response.headers["Location"] = "/welcome"
-        response.status_code = status.HTTP_302_FOUND 
+        s_token = sha256(
+            bytes(f"{correct_username}{correct_password}", encoding="utf8")
+        ).hexdigest()
+        app.tokens += s_token
+        response.set_cookie(key="session_token", value=s_token)
+        response.headers["Location"] = "/welcome"
+        response.status_code = status.HTTP_302_FOUND
     return response
+
 
 @app.post("/logout")
 def logout(*, response: Response, session_token: str = Cookie(None)):
-	if session_token not in app.tokens:
-		raise HTTPException(status_code=401, detail="Unathorised")
-	app.tokens.delete(session_token)
-	return RedirectResponse("/")
+    if session_token not in app.tokens:
+        raise HTTPException(status_code=401, detail="Unathorised")
+    app.tokens.delete(session_token)
+    return RedirectResponse("/")
